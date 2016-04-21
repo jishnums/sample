37380e1abaf010cfb12b5a34f1292706a9c88ffa
Jishnu MS <jishnusivan7@gmail.com>
2016-04-21 07:47:26 +0530

    sample commit

---
+++ src/HelloForm.java
@@ -0,0 +1,28 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class HelloForm extends HttpServlet {
+    public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException
+    {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        String title = "Using GET Method to Read Form Data";
+        String docType =
+                "<!doctype html public \"-//w3c//dtd html 4.0 " +
+                        "transitional//en\">\n";
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor=\"#f0f0f0\">\n" +
+                "<h1 align=\"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                "  <li><b>Parameter Names</b>: "+request.getParameterNames()+
+
+                "</body></html>");
+    }
+}
